2a42ba8f1268c99653b1d5b8c40008a6d9c3e70c
Kevin Camargo <75258167+KevinARCamargo@users.noreply.github.com>
2024-01-16 11:19:46 -0300

    add hamburgueria mobile

---
+++ projetos/Hamburgueria/script/script.js
@@ -0,0 +1,10 @@
+function mostrarDetalhes(cardapio) {
+    var detalhes = cardapio.querySelector('.detalhes');
+    detalhes.style.display = detalhes.style.display === 'none' ? 'block' : 'none';
+  }
+  
+  function adicionarAoCarrinho() {
+    // Lógica para adicionar ao carrinho aqui
+    alert('Produto adicionado ao carrinho!');
+  }
+  
