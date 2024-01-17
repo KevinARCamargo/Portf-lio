622663ff00b57de60eda4df1e25aaec4777cb0f4
Kevin Camargo <75258167+KevinARCamargo@users.noreply.github.com>
2024-01-17 11:04:20 -0300

    jogo da memoria responsivo

--- script/script.js
+++ script/script.js
@@ -3,47 +3,18 @@ document.addEventListener("DOMContentLoaded", function() {
   main.classList.add('aparece');
 });
 
-// Definindo as funções no escopo global
-let slideIndex = 1;
 
-function mudarSlide(n) {
-  showSlides(slideIndex += n);
+function showTooltip(tech) {
+  var tooltip = document.querySelector('.magica');
+  tooltip.innerHTML = tech;
+  tooltip.style.display = 'block';
+  tooltip.style.opacity = 1; 
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function hideTooltip() {
+  var tooltip = document.querySelector('.magica');
+  tooltip.style.opacity = 0; 
+  setTimeout(() => {
+      tooltip.style.display = 'none';
+  }, 300); 
 }
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("carrossel-slide");
-  let indicadores = document.getElementsByClassName("carrossel-indicador");
-
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-
-  for (i = 0; i < indicadores.length; i++) {
-    indicadores[i].className = indicadores[i].className.replace(" active", "");
-  }
-
-  slides[slideIndex - 1].style.display = "block";
-  indicadores[slideIndex - 1].className += " active";
-}
-
-// Aguardando o DOM ser carregado
-document.addEventListener("DOMContentLoaded", function() {
-  const main = document.querySelector('.cartao');
-  main.classList.add('aparece');
-
-  // Chamando a função para iniciar o carrossel
-  showSlides(slideIndex);
-});
